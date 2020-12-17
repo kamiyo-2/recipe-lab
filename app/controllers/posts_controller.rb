eaa25351414ef4495c30530388a16186d9ab0c51
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-17 17:21:23 +0900

    comment機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.includes(:user)
   end
@@ -21,6 +21,10 @@ class PostsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   private
