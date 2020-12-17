2144887def38c9bc0cefa81492bfdbc5ea0b267c
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-17 13:42:15 +0900

    バリデーション 設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :email,           format: { with: /@/}, on: :create
+         validates :password,        length: { minimum: 6 }, confirmation: true,  format: { with: /\A[a-zA-Z0-9]+\z/ }, on: :create
+       
+         validates :name, presence: true
+         validates :profile, presence: true       
 end
